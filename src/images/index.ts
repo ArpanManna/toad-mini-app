ac1e526e683fba730642329d76ce0d40cec215fc
AnanyaSshri <shrivastava.ananya26@gmail.com>
2024-08-19 19:17:25 +0530

    unnecessary assets removed

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,23 +1,7 @@
-import hamsterExchange from "./hamster-exchange.png";
-import binanceLogo from "./binance-logo.png";
-import dollarCoin from "./dollar-coin.png";
-import dailyReward from "./daily-reward.png";
-import dailyCipher from "./daily-cipher.png";
-import dailyCombo from "./daily-combo.png";
-import mainCharacter from "./main-character.png";
-import hamsterCoin from "./hamster-coin.png";
 import toadIcon from "../assets/toad_svg.svg"
 import leaderBoard from './leaderboard.png'
 import leaderBoard1 from "./leaderboard1.png";
 export {
-    hamsterExchange,
-    binanceLogo,
-    dollarCoin,
-    dailyReward,
-    dailyCipher,
-    dailyCombo,
-    mainCharacter,
-    hamsterCoin,
     toadIcon,
     leaderBoard,
     leaderBoard1
