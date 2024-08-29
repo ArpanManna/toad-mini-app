58ef9e9c5bc26e429914c88f635f2266aa18c2e2
AnanyaSshri <shrivastava.ananya26@gmail.com>
2024-08-29 22:56:34 +0530

    channel link updated

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const config = {
     'telegramApiUrl': 'https://api.telegram.org',
     'joiningBonus': '500',
     'community_group_link': 'https://t.me/+qGL8DC4Iz3VkNGQ1',
-    'community_channel_link': 'https://t.me/joinToad',
+    'community_channel_link': 'https://t.me/toad_community_ton',
     'referralBonus': '250'
 }
